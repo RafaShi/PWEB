b86eb669404db9d3da2a9c7ab4772b3ebf16cbee
RafaShi <rafayumishi@gmail.com>
2025-10-13 19:56:11 -0300

    .

---
+++ EstudosPrj_JS/Estudos.js
@@ -0,0 +1,27 @@
+document.getElementById ("texto").innerHTML="Meu primeiro texto <b>js<b>!";
+
+alert("Oi, isso é um alert");
+
+console.log("Oi, isso é um console.log")
+
+
+let a, b, c;
+a=5;
+b=6;
+c=7;
+
+//função
+function minhafuncao(){
+    document.write(5+5);
+    document.write(5+5);
+    document.write(5+5);
+}
+
+console.log(5+5);
+
+var x, y, z;
+x=2;
+y=3;
+z= a+b;
+alert(z);
+//5
